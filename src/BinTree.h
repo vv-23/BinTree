a8da565aaf6a95416212d8e396af4cbbc09e0ccf
Streeter <streeter@student.clackamas.edu>
2019-05-08 16:52:23 -0700

    Functionality done, add comments

--- src/BinTree.h
+++ src/BinTree.h
@@ -9,14 +9,17 @@ class StringBinTree {
 public:
     StringBinTree();
 
-    bool insert(std::string data);
+    bool insert(const std::string& data);
     
     void print(std::ostream& stream);
 
     //bool isEmpty();
+
+	bool remove(const std::string& data);
 private:
     bool insert(const NodePtr<std::string>& newNode, NodePtr<std::string>& current);
-    void print(std::ostream& stream, NodePtr<std::string> current);
+    void print(std::ostream& stream, NodePtr<std::string> current, int level);
+	void removeNode(NodePtr<std::string>& node);
     NodePtr<std::string> root;
 };
 
@@ -25,6 +28,72 @@ StringBinTree::StringBinTree() {
     root = nullptr;
 };
 
+
+bool StringBinTree::remove(const std::string & data) {
+	NodePtr<std::string> current = root;
+	NodePtr<std::string> parent = nullptr;
+	bool found = false;
+	
+	while (current != nullptr && !found) {
+		if (data == current->data) {
+			found = true;
+		}
+		else {
+			parent = current;
+			if (data < current->data) current = current->leftChild;
+			else current = current->rightChild;
+		}
+	}
+	if (found) {
+		if (current == root) removeNode(root);
+		else if (data < parent->data) removeNode(parent->leftChild);
+		else removeNode(parent->rightChild);
+		return true;
+	}
+	return false;
+}
+
+void StringBinTree::removeNode(NodePtr<std::string>& node) {
+	if ((node->leftChild == nullptr) && (node->rightChild == nullptr)) {
+		//Decrease the reference counter by 1. When the counter reaches 0 (typically when "current" from remove method goes out of scope), the object will automatically be destroyed
+		node = nullptr;
+	}
+
+	else if (node->rightChild == nullptr) { // Node has left child
+		/*node now points to the left child of the Node object that it handles. When no more pointers own that object, the object is destroyed
+		(*node->leftChild) now has 2 pointers pointing to it: node and node->leftChild. When the object that node previously owned is destroyed
+		(typically when "current" from remove method goes out of scope), its leftChild destructor will be called, decreasing the refcount to its leftChild object by 1
+		But now node is pointing at that object, it won't be removed.
+		*/
+		node = node->leftChild;
+	}
+
+	else if (node->leftChild == nullptr) { // Node has right child
+		//Same as left child
+		node = node->rightChild;
+	}
+
+	else { //Has 2 children
+		NodePtr<std::string> current = node->leftChild;
+		NodePtr<std::string> parent = nullptr;
+
+		//find suitable replacement node by going left once and go right as deep as possible
+		while (current->rightChild != nullptr) {
+			parent = current;
+			current = current->rightChild;
+		}
+		node->data = current->data;
+
+		/*if replacement data is from node's left child: node->leftChild now points to current->leftChild, decreasing (*node->leftChild) ref count by 1
+		When current goes out of scope, no more pointers will be pointing at the object mentioned above and it will be destroyed
+		When that object is destroyed, its leftChild destructor is called, decreasing the ref count by 1
+		But that leftChild still have node->leftChild pointing to it, so it will not be destroyed*/
+		if (parent == nullptr) node->leftChild = current->leftChild;
+
+		//replacement node comes from further down
+		else parent->rightChild = current->leftChild;
+	}
+}
 
 bool StringBinTree::insert(const NodePtr<std::string>& newNode, NodePtr<std::string>& current) {
     if (current==nullptr) {
@@ -38,22 +107,22 @@ bool StringBinTree::insert(const NodePtr<std::string>& newNode, NodePtr<std::str
     return true;
 };
 
-bool StringBinTree::insert(std::string data) {
+bool StringBinTree::insert(const std::string& data) {
     NodePtr<std::string> newNode = std::make_shared<Node<std::string>>();
     newNode->data = data;
-    insert(newNode, root);
+    return insert(newNode, root);
 }
 
 
 void StringBinTree::print(std::ostream& stream) {
-    print(stream, root);
+    print(stream, root,0);
 };
 
-void StringBinTree::print(std::ostream& stream, NodePtr<std::string> current) {
+void StringBinTree::print(std::ostream& stream, NodePtr<std::string> current, int level) {
     if (current==nullptr) return;
     else {
-        print(stream, current->leftChild);
-        stream << current->data << " ";
-        print(stream, current->rightChild);
+        print(stream, current->leftChild, level+1);
+        stream << current->data << "(" << level << ") ";
+        print(stream, current->rightChild, level+1);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,46 @@ using namespace std;
 
 
 int main() {
+	cout << "CS260, A5-Binary Trees\nAuthor:Vi Vuong\n\n";
+
     StringBinTree s;
-    s.insert("B");
-    s.insert("A");
-    s.insert("C");
-    s.print(cout);
+	
+	s.insert("Jynx");
+	s.insert("Charmander");
+	s.insert("Snorlax");
+	s.insert("Diglett");
+	s.insert("Kakuna");
+	s.insert("Meowth");
+	s.insert("Pikachu");
+	s.insert("Blastoise");
+	s.insert("Squirtle");
+	s.insert("Ivysaur");
+	s.insert("Bulbasaur");
+	s.insert("Abra");
+	s.print(cout);
+
+	cout << endl << endl;
+
+
+	cout << "Deleting Squirtle\n";
+	s.remove("Squirtle");
+	s.print(cout);
+	cout << endl << endl;
+
+	cout << "Deleting Meowth\n";
+	s.remove("Meowth");
+	s.print(cout);
+	cout << endl << endl;
+
+	cout << "Deleting Blastoise\n";
+	s.remove("Blastoise");
+	s.print(cout);
+	cout << endl << endl;
+
+	cout << "Deleting Jynx\n";
+	s.remove("Jynx");
+	s.print(cout);
+	cout << endl << endl;
+    
     return 0;
 }
